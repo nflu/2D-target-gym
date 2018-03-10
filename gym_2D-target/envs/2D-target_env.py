cb4ae749700d77edc6be1cc4700a7a160632cf56
nflu <nflugovoy@berkeley.edu>
2018-03-10 01:09:09 -0800

    fit to gym specifications

--- gym_2D-target/envs/2D-target_env.py
+++ gym_2D-target/envs/2D-target_env.py
@@ -1,32 +1,29 @@
 import numpy as np
 import gym 
-from gym import spaces 
+from gym import spaces
 
-class 2D-target_env(gym.Env):
+class target_2D_env(gym.Env):
 
 	def __init__(self):
 		
 		# defines size of environment, dimension, and time step size
-		self.stepSize = 0.5 
-		self.xlim = np.array([-200.0, 200.0])
-		self.ylim = np.zeros([-200.0, 200.0])
+		self.timeHorizon = 500
+		self.stepSize = 1
+		boxSizeX = 100
+		boxSizeY = 100
+		self.high = np.array([boxSizeX, boxSizeY])
 		self.stateDim = 3 #simple version only takes steps for now
-		#must be at least 3, (x, y, lambda)
+		#must be at least 3 : (x, y, lambda)
+		#if the dimension is increased and the new dimensions will be used in the observation
+		#the dimension of the box must also increase.
 
-		self.targetPoint = self.randomPoint()
-		def l(point):
-			return np.linalg.norm(point - targetPoint)
-
-		self.target = l
-		self.state = self.randomPoint()
 		self.action_space = spaces.Discrete(5) #left, right, down, up, don't move
-		self.curr_step = 0
-		self.lastLambda = 0 
+		self.observation_space = spaces.Box(low = -self.high, high = self.high, dtype = np.float32)
 
 	def randomPoint(self):
 		point = np.zeros(2)
-		self.point[0] = np.random.uniform(xlim[0], xlim[1])
-		self.point[1] = np.random.uniform(ylim[0], ylim[1])
+		self.point[0] = np.random.randint(low = -self.high[0], high = self.high[0]) #for now this will be gridworld
+		self.point[1] = np.random.randint(low = -self.high[0], high = self.high[0])
 		return point
 
 	def randomState(self):
@@ -36,17 +33,35 @@ class 2D-target_env(gym.Env):
 		else:
 			return np.append(point, np.zeros(stateDim - 3), self.target(point))
 
+	def _get_reward(self):
+		return self.state[-1] - self.lastLambda
+
 	def _step(self, action):
 		self._take_action(action)
 		self.curr_step += 1
-		reward = self.state[-1] - self.lastLambda
-		ob = np.append(self.state, self.targetPoint)
-		episode_over = reward == 0 
+		reward = self._get_reward()
+		ob = self.state[:len(high)] 
+		episode_over = self.curr_step >= self.timeHorizon
 		return ob , reward, episode_over, {}
 
+	def _reset(self):
+		self.targetPoint = self.randomPoint()
+		def l(point):
+			return np.linalg.norm(point - targetPoint)
+
+		self.target = l
+		self.state = self.randomState()
+		self.curr_step = 0
+		self.lastLambda =  float("inf")
+		ob = np.append(self.state, self.targetPoint)
+		return ob
 
 	def _take_action(self, action):
 		if action != 5:
 			self.state[action/2] += 2((action%2)-0.5)*stepSize
 		self.lastLambda = self.state[-1]
 		self.state[-1] = np.min(self.state[-1], target(self.state[:2]))
+
+test = target_2D_env()
+test._reset
+test._step
