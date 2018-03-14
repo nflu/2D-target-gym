d22e6b713204660fe5c09e346ef7da607abc60e4
nflu <nflugovoy@berkeley.edu>
2018-03-14 13:34:56 -0700

    updated run file. About to rename everything.

--- run_2D_target.py
+++ run_2D_target.py
@@ -1,13 +1,15 @@
-from mpi4py import MPI
-from baselines.common import set_global_seeds
-from baselines import bench
-import os.path as osp
+from baselines.common import tf_util as U
 from baselines import logger
+from baselines.ppo1 import mlp_policy, pposgd_simple
 import gym
-import target_2D #TODO this might need to be updated
+import  #TODO this might need to be updated
 
 def train(env_id, num_timesteps, seed):
-#TODO
+	U.make_session(num_cpu=1).__enter__()
+	def policy_fn(name, ob_space, ac_space):
+		return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+			hid_size = 64, num_hid_layers = 2)
+	env = gym.make()
 
 def main():
 #TODO
