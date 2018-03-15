dbb80f9e8140d81c17ee7db6c593b8fe9d6cb7b7
nflu <nflugovoy@berkeley.edu>
2018-03-14 17:19:02 -0700

    updated script and fixed environment. working on rendering right now

--- gym_target/envs/target_env.py
+++ gym_target/envs/target_env.py
@@ -9,63 +9,72 @@ class TargetEnv(gym.Env):
 		# defines size of environment, dimension, and time step size
 		self.timeHorizon = 500
 		self.stepSize = 1
-		boxSizeX = 100
-		boxSizeY = 100
-		self.high = np.array([boxSizeX, boxSizeY])
+		boxSizeX = 100.0
+		boxSizeY = 100.0
+		maxLambda = np.sqrt((2*boxSizeX)**2 + (2*boxSizeY)**2)
+		self.high = np.array([boxSizeX, boxSizeY, maxLambda, boxSizeX, boxSizeY])
+		#note that this describes the limits on the observation space.
+		#right now the observation space is the state vector concatenated with the target set point
 		self.stateDim = 3 #simple version only takes steps for now
 		#must be at least 3 : (x, y, lambda)
 		#if the dimension is increased and the new dimensions will be used in the observation
 		#the dimension of the box must also increase.
-
 		self.action_space = spaces.Discrete(5) #left, right, down, up, don't move
 		self.observation_space = spaces.Box(low = -self.high, high = self.high, dtype = np.float32)
 
 	def randomPoint(self):
 		point = np.zeros(2)
-		self.point[0] = np.random.randint(low = -self.high[0], high = self.high[0]) #for now this will be gridworld
-		self.point[1] = np.random.randint(low = -self.high[0], high = self.high[0])
+		point[0] = np.random.randint(low = -self.high[0], high = self.high[0]) #for now this will be gridworld
+		point[1] = np.random.randint(low = -self.high[0], high = self.high[0])
 		return point
 
 	def randomState(self):
 		point = self.randomPoint()
-		if (stateDim == 3):
+		if (self.stateDim == 3):
 			return np.append(point, self.target(point))
 		else:
 			return np.append(point, np.zeros(stateDim - 3), self.target(point))
 
 	def _get_reward(self):
-		return self.lastLambda - self.state[-1] #formulation is reverse so that reward can be maximized
+		return self.lastLambda - self.state[-1] #formulation is negated so that reward can be maximized
 
 	def _step(self, action):
 		self._take_action(action)
 		self.curr_step += 1
 		reward = self._get_reward()
-		ob = self.state[:len(high)] 
+		ob = np.append(self.state, self.targetPoint)
 		episode_over = self.curr_step >= self.timeHorizon
 		return ob , reward, episode_over, {}
 
 	def _reset(self):
 		self.targetPoint = self.randomPoint()
 		def l(point):
-			return np.linalg.norm(point - targetPoint)
+			return np.linalg.norm(point - self.targetPoint)
 
 		self.target = l
 		self.state = self.randomState()
 		self.curr_step = 0
 		self.lastLambda =  float("inf")
+		self.viewer = None
 		ob = np.append(self.state, self.targetPoint)
 		return ob
 
 	def _take_action(self, action):
 		if action != 5:
-			self.state[action/2] += 2((action%2)-0.5)*stepSize 
+			self.state[action//2] += 2 * ((action % 2)-0.5)*self.stepSize 
 		self.lastLambda = self.state[-1]
-		self.state[-1] = np.min(self.state[-1], target(self.state[:2]))
+		self.state[-1] = min(self.state[-1], self.target(self.state[:2]))
 
-	def _render(self):
-		#TODO
-		pass
+	def _render(self, mode="human", close=False):
+		from gym.envs.classic_control import rendering
+		if self.viewer is None:
+			self.viewer = rendering.Viewer(2*self.high[0], 2*self.high[1])
+			self.viewer.set_bounds(-self.high[0], self.high[0], -self.high[1], self.high[1])
+			target = rendering.make_circle(1)
+			target.add_attr(rendering.Transform(translation=self.targetPoint))
 
-	def seed(self):
+		return self.viewer.render(return_rgb_array = mode=='rgb_array')
+
+	def _seed(self):
 		#TODO
 		pass
--- ppo_run_target.py
+++ ppo_run_target.py
@@ -10,11 +10,15 @@ def train(env_id, num_timesteps, seed):
 	U.make_session(num_cpu=1).__enter__()
 	def policy_fn(name, ob_space, ac_space):
 		return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
-			hid_size = 64, num_hid_layers = 2)
+			hid_size = 64, num_hid_layers = 3)
 	env = gym.make("target-v0")
 	pposgd_simple.learn(env, policy_fn, 
 		max_timesteps = num_timesteps,
-		gamma=1.0)
+		gamma=1.0, timesteps_per_actorbatch = 128,
+		clip_param=0.2, entcoeff=0.0,
+            optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
+             lam=0.95, schedule='linear',
+        )
 	env.close()
 
 def main():
