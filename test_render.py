a4805b5d181910f6525d716e260da106068230d0
nflu <nflugovoy@berkeley.edu>
2018-03-16 02:17:51 -0700

    updated render test script

--- test_render.py
+++ test_render.py
@@ -2,11 +2,12 @@ import gym
 import gym_target
 env = gym.make('target-v0')
 env._reset()
-while True:
+for _ in range(1):
 	a = env.action_space.sample()
 	print(a)
 	env._step(a)
 	print(env.state)
 	for _ in range(100):
 		env._render()
-env.close()
+print("before close")
+env._close()
