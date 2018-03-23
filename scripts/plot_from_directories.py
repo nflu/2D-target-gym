4074d560357e7fedd5794e12fd4c6a7c50f054ea
nflu <nflugovoy@berkeley.edu>
2018-03-23 15:54:26 -0700

    updated hyper-parameters and fixed plotting script

--- scripts/plot_from_directories.py
+++ scripts/plot_from_directories.py
@@ -1,5 +1,7 @@
+import sys
+sys.path.insert(0, '/home/trustlap41/gym-target/openai_modified/')
+import argparse
 import results_plotter_terminal
-import argparse 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("file_name", type=str)
@@ -7,7 +9,8 @@ def main():
 	parser.add_argument("task", type=str)
 	args = parser.parse_args()
 	with open(args.file_name) as f:
-		results_plotter_terminal.plot_results(f, args.num_timesteps, results_plotter_terminal.X_TIMESTEPS, args.task) 
+		for line in f:
+			results_plotter_terminal.plot_results([line], args.num_timesteps, results_plotter_terminal.X_TIMESTEPS, args.task) 
 
 if __name__ == '__main__':
 	main()
--- trpo_run_target.py
+++ trpo_run_target.py
@@ -2,26 +2,25 @@ from baselines.common import tf_util as U
 from baselines import logger
 from baselines.ppo1 import mlp_policy
 from baselines import bench
-from openai_modified import pposgd_simple_render
+from baselines.trpo_mpi import trpo_mpi
 import gym
 import gym_target
 import argparse
 from  openai_modified import results_plotter_terminal
 
 
-def train(env_id, num_timesteps, render_timesteps, render_time, seed):
+def train(env_id, num_timesteps, seed):
 	U.make_session(num_cpu=1).__enter__()
 	def policy_fn(name, ob_space, ac_space):
 		return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
 			hid_size = 64, num_hid_layers = 2)
 	logger.configure()
 	env = bench.Monitor(gym.make("target-v0"), logger.get_dir())
-	pposgd_simple_render.learn(env, policy_fn, 
-		max_timesteps = num_timesteps, render_timesteps = render_timesteps,
-		render_time = render_time, gamma=1.0, timesteps_per_actorbatch = 5000,
-		clip_param=0.10, entcoeff=0.02,
-            optim_epochs=10, optim_stepsize=5e-4, optim_batchsize=64,
-             lam=0.95, schedule='linear'
+	trpo_mpi.learn(env, policy_fn, 
+		max_timesteps = num_timesteps, gamma=1.0, timesteps_per_batch = 5000,
+		max_kl=0.01, entcoeff=0.02,
+            vf_iters=5, vf_stepsize=1e-3,
+             lam=0.95, cg_iters=10, cg_damping=0.1,
         )
 	dir  = logger.get_dir()
 	with open("data/data_directories.txt", 'a') as file:
@@ -34,10 +33,8 @@ def train(env_id, num_timesteps, render_timesteps, render_time, seed):
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("num_timesteps", type=float)
-	parser.add_argument("render_timesteps", type=float , help="will render these last many timesteps")
-	parser.add_argument("render_time", type=float , help="will render each frame for at least this much time")
 	args = parser.parse_args()
-	train(None, args.num_timesteps, args.render_timesteps, args.render_time, None)
+	train(None, args.num_timesteps, None)
 
 if __name__ == '__main__':
 	main()
