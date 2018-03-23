cf124c504a90d455c1557f44ad87ccc8c5c14ff5
nflu <nflugovoy@berkeley.edu>
2018-03-23 02:45:15 -0700

    updated setup and init files

--- gym_target/__init__.py
+++ gym_target/__init__.py
@@ -4,3 +4,7 @@ register(
 	id="target-v0",
 	entry_point="gym_target.envs:TargetEnv",
 )
+register(
+	id="target-dynamics-v0",
+	entry_point="gym_target.envs.TargetDynamicsEnv",
+)
--- gym_target/envs/__init__.py
+++ gym_target/envs/__init__.py
@@ -1,1 +1,2 @@
 from gym_target.envs.target_env import TargetEnv
+from gym_target.envs.target_dynamics_env import TargetDynamicsEnv
