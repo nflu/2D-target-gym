5552ec781000cfb6853af37a87dfc1765a21c8ca
Neil Lugovoy <nflugovoy@berkeley.edu>
2018-03-27 18:30:19 -0700

    fixed plotting

---
+++ scripts/plot.py
@@ -0,0 +1,46 @@
+#using the openai plotting script became too much of a pain so made this and it works much better
+
+import sys
+#sys.path.insert(0, '/home/trustlap41/gym-target/openai_modified/')
+sys.path.insert(0, '/Users/Neil/gym-target/openai_modified/')
+import argparse
+import numpy as np 
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt 
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("directory_file", type=str, help="this is a text file with the directories of data separated by new lines")
+	parser.add_argument("-f", "--data_file", type=str, help="Plots data from files with this name in the listed directories. Default is monitor.csv") #optional
+    #need to type -f before argument in command line
+	args = parser.parse_args()
+	if args.data_file == None:
+		args.data_file = "monitor.csv"
+
+	with open(args.directory_file) as f:
+		l = []
+		for line in f:
+			line = line.strip() #need to remove whitespace at end of file name
+			line += "/" + args.data_file
+			l.append(line)
+		i = 0
+		for file in l:
+			data = get_reward(np.genfromtxt(file, delimiter=',', skip_header=2,
+                     skip_footer=10, names=['r', 'l', 't']))
+			plt.figure(i)
+			plt.plot(data)
+			plt.xlabel('time steps taken')
+			plt.ylabel('reward')
+			plt.title(file)
+			i+=1
+	plt.show()
+
+def get_reward(data):
+	rewards = np.zeros(len(data))
+	for i in range(len(data)):
+		rewards[i] = data[i][0]
+	return rewards
+	
+if __name__ == '__main__':
+	main()
--- scripts/plot_from_directories.py
+++ scripts/plot_from_directories.py
@@ -15,7 +15,7 @@ def main():
 	with open(args.file_name) as f:
 		l = []
 		for line in f:
-			line = line[:-1] #need to remove whitespace at end of file name
+			line = line.strip() #need to remove whitespace at end of file name
 			l.append(line)
 		#TODO find the set of tasks and then group files according to task and number of time steps and then plot each like that
 		results_plotter_terminal.plot_results(l, args.num_timesteps, results_plotter_terminal.X_TIMESTEPS, args.task) 
