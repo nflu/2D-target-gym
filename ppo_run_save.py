122dd818fa6df2e0217da4b6a4b33934da4e828d
Neil Lugovoy <nflugovoy@berkeley.edu>
2018-04-23 23:29:57 -0700

    changes for experiment

--- ppo_run_save.py
+++ ppo_run_save.py
@@ -4,17 +4,27 @@ from baselines.ppo1 import mlp_policy
 from baselines import bench
 from openai_modified import pposgd_simple_save
 from openai_modified import beta_policy
+from openai_modified import sigmoid_policy
 import gym
 import gym_target
 import argparse
 from openai_modified import results_plotter_terminal
 
 #TODO put in some check that save_prefix is valid so ppo doesn't crash
-def train(env_id, num_timesteps, save_prefix, restore_file, task, seed):
+def train(num_timesteps, save_prefix, restore_file, task, policy, graph=True):
 	U.make_session(num_cpu=1).__enter__()
 	def policy_fn(name, ob_space, ac_space):
-		return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
+		if policy == "sigmoid":
+			return sigmoid_policy.SigmoidPolicy(name = name, ob_space=ob_space, ac_space=ac_space,
+			hid_size = 32, num_hid_layers=3)
+		elif policy == "mlp":
+			return mlp_policy.MlpPolicy(name=name, ob_space=ob_space, ac_space=ac_space,
 			hid_size = 32, num_hid_layers = 3)
+		elif policy == "beta":
+			return beta_policy.BetaPolicy(name = name, ob_space=ob_space, ac_space=ac_space,
+			hid_size = 32, num_hid_layers=3)
+
+
 	logger.configure()
 	if task == "grid":
 		env = bench.Monitor(gym.make("target-v0"), logger.get_dir())
@@ -22,7 +32,7 @@ def train(env_id, num_timesteps, save_prefix, restore_file, task, seed):
 		env = bench.Monitor(gym.make("target-dynamics-v0"), logger.get_dir())
 	else:
 		raise ValueError("task should be either grid or dynamics instead " + task + " was given.")
-	pposgd_simple_save.learn(env, policy_fn, 
+	pposgd_simple_save.learn(env, policy_fn,
 		max_timesteps = num_timesteps, save_model_with_prefix=save_prefix, restore_model_from_file=restore_file, gamma=1.0, timesteps_per_actorbatch = 10000,
 		clip_param=0.10, entcoeff=0.02,
 			optim_epochs=10, optim_stepsize=1e-4, optim_batchsize=64,
@@ -33,19 +43,22 @@ def train(env_id, num_timesteps, save_prefix, restore_file, task, seed):
 		file.write(dir+"\n")
 	with open(dir + "/metadata.txt", 'w') as file:
 		file.write(task + "," + str(num_timesteps))
-	results_plotter_terminal.plot_results([dir], num_timesteps, results_plotter_terminal.X_TIMESTEPS, "Target Set " + task) 
-	#for this to have at least 99 episodes. See line 20 in rolling_window in results_plotter.py from open ai baselines 
+	if graph:
+		results_plotter_terminal.plot_results([dir], num_timesteps, results_plotter_terminal.X_TIMESTEPS, "Target Set " + task)
+	#for this to have at least 99 episodes. See line 20 in rolling_window in results_plotter.py from open ai baselines
 	#If there are too few samples, that dimension will be negative and numpy will cause this to crash
 	env.close()
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("num_timesteps", type=float)
-	parser.add_argument("task", type=str)
+	parser.add_argument("task", type=str, help="grid or dynamics")
 	parser.add_argument("save_prefix", type=str, help="prefix of saved model name")
 	parser.add_argument("-r", "--restore_file", type=str)
+	parser.add_argument("-p", "--policy", type=str, help="mlp, sigmoid, beta")
+	parser.add_argument("-g", "--graph", type=bool)
 	args = parser.parse_args()
-	train(None, args.num_timesteps, args.save_prefix, args.restore_file, args.task, None)
+	train(args.num_timesteps, args.save_prefix, args.restore_file, args.task, args.policy, args.graph)
 
 if __name__ == '__main__':
 	main()
